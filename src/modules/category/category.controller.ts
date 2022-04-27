fe39799e72064264f091456038582b565e370087
hannd <hannd@runsystem.net>
2022-04-27 17:51:13 +0700

    add role for api category

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -9,6 +9,7 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { DeleteResult, UpdateResult } from 'typeorm';
@@ -16,11 +17,18 @@ import { CreateCategoryDto } from './dtos/create-category.dto';
 import { UpdateCategoryDto } from './dtos/update-category.dto';
 import { ICategoryEntity } from './category.entity';
 import { CategoryRepository } from './category.repository';
+import { Constants } from '../../core/enum/constants.enum';
+import { JwtOAuthGuard } from '../../core/guards/jwt.guard';
+import { RolesGuard } from '../../core/guards/roles.guard';
+import { Roles } from '../../core/decorators/roles.decorator';
 
 @Controller('categories')
 @ApiTags('category')
 export class CategoryController {
   constructor(private categoryRepository: CategoryRepository) {}
+
+  @UseGuards(JwtOAuthGuard, RolesGuard)
+  @Roles([Constants.IS_ADMIN])
   @Post()
   @HttpCode(HttpStatus.CREATED)
   async createCategory(@Body() createCategoryDto: CreateCategoryDto) {
@@ -29,6 +37,7 @@ export class CategoryController {
     this.categoryRepository.save(category);
   }
 
+  @UseGuards(JwtOAuthGuard, RolesGuard)
   @HttpCode(HttpStatus.OK)
   @Get()
   async getAllCategory(): Promise<ICategoryEntity[]> {
@@ -40,6 +49,7 @@ export class CategoryController {
     });
   }
 
+  @UseGuards(JwtOAuthGuard, RolesGuard)
   @Get(':id')
   @ApiParam({
     name: 'id',
@@ -54,6 +64,8 @@ export class CategoryController {
     return category;
   }
 
+  @UseGuards(JwtOAuthGuard, RolesGuard)
+  @Roles([Constants.IS_ADMIN])
   @Put(':id')
   @ApiParam({
     name: 'id',
@@ -67,6 +79,8 @@ export class CategoryController {
     return this.categoryRepository.update(params.id, updateCategoryDto);
   }
 
+  @UseGuards(JwtOAuthGuard, RolesGuard)
+  @Roles([Constants.IS_ADMIN])
   @Delete(':id')
   @ApiParam({
     name: 'id',
