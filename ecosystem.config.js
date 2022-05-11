723d8fe461629cbcf7e5510bec4de7afd6ee9efa
rs-cuongph <cuongph@runsystem.net>
2022-05-11 08:23:59 +0700

    update ci

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,13 @@ module.exports = {
     apps: [
       {
         name: 'picoworker_backend',
-        script: 'node ./main.js',
+        script: 'node dist/src/main.js',
         time: true,
         instances: 1,
         autorestart: true,
         max_restarts: 10,
         watch: [
-            './main.js'
+            'dist/src/main.js'
         ],
         max_memory_restart: '1G',
         env: {
