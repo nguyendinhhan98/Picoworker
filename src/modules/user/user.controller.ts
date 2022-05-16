96fcd065f63742abc727311b19caa9a87811d987
hannd <hannd@runsystem.net>
2022-05-16 13:50:43 +0700

    completed api switch role

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -9,6 +9,7 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { REGIST_ROLES } from 'src/core/enum/constants.enum';
 import { JwtOAuthGuard } from 'src/core/guards/jwt.guard';
 import { UpdateUserPasswordDto } from './dtos/update-user-password.dto';
 import { IUserEntity } from './user.entity';
@@ -24,7 +25,7 @@ export class UserController {
   @Get('/info')
   @HttpCode(HttpStatus.OK)
   async getInfoUser(@Request() req): Promise<IUserEntity> {
-    return await this.userRepository.getUserByEmail(req.user.email);
+    return await this.userRepository.getUserById(req.user.id);
   }
 
   @UseGuards(JwtOAuthGuard)
@@ -36,5 +37,18 @@ export class UserController {
     @Request() req,
   ) {
     return this.userRepository.changePassword(req.user, updateUserPasswordDto);
+  }
+
+  @UseGuards(JwtOAuthGuard)
+  @ApiBearerAuth()
+  @Post('/switch-role')
+  @HttpCode(HttpStatus.ACCEPTED)
+  async switchRole(@Request() req) {
+    const user = await this.getInfoUser(req);
+    const newRole =
+      user.role === REGIST_ROLES.WORKER
+        ? REGIST_ROLES.EMPLOYER
+        : REGIST_ROLES.WORKER;
+    await this.userRepository.update(req.user.id, { role: newRole });
   }
 }
