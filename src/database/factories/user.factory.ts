a8fb88cf64ca044892441416b81c558a6e5f3f46
rs-cuongph <cuongph@runsystem.net>
2022-10-05 18:52:54 +0700

    fix build

--- src/database/factories/user.factory.ts
+++ src/database/factories/user.factory.ts
@@ -1,15 +1,15 @@
-import * as Faker from 'faker';
+import { faker } from '@faker-js/faker';
 import { UserEntity } from '../../modules/user/user.entity';
 import { define } from 'typeorm-seeding';
 
-define(UserEntity, (faker: typeof Faker) => {
+define(UserEntity, () => {
   const user = new UserEntity();
-  user.id = faker.random.uuid();
-  user.fullName = faker.name.findName();
+  user.id = faker.datatype.uuid();
+  user.fullName = faker.name.fullName();
   user.email = faker.internet.email();
   user.username = faker.internet.userName();
   user.password = 'Abc@123123';
   user.region = faker.address.country();
-  user.role = faker.random.arrayElement(['ADMIN', 'WORKER', 'EMPLOYER']);
+  user.role = faker.helpers.arrayElement(['ADMIN', 'WORKER', 'EMPLOYER']);
   return user;
 });
